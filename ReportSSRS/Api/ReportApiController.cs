4f302d8124b3465c46386d7d724cfa69527a5af1
Kingbede <Kokou.Kingbede@mail.nidec.com>
2017-11-07 10:38:20 -0600

    SSRS new project

---
+++ ReportSSRS/Api/ReportApiController.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.EJ.ReportViewer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReportSSRS.Api
+{
+    public class ReportApiController : ApiController, IReportController 
+    {
+        //Post action for processing the rdl/rdlc report 
+        public object PostReportAction(Dictionary < string, object > jsonResult )
+        {
+            return ReportHelper.ProcessReport(jsonResult, this);
+        }
+        
+        //Get action for getting resources from the report
+        [System.Web.Http.ActionName("GetResource")]
+        [AcceptVerbs("GET")]
+        public object GetResource(string key, string resourcetype, bool isPrint) 
+        {
+            return ReportHelper.GetResource(key, resourcetype, isPrint);
+        }
+        
+        //Method will be called when initialize the report options before start processing the report        
+        public void OnInitReportOptions(ReportViewerOptions reportOption)
+        {
+            //You can update report options here
+        }
+        
+        //Method will be called when reported is loaded
+        public void OnReportLoaded(ReportViewerOptions reportOption) 
+        {
+            //You can update report options here
+        }
+    }
+}
